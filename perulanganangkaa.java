c267e9d25260dc1adc57d696c08daa57f802fc0a
Risfan Novrian <93021736+risfannovrian18@users.noreply.github.com>
2021-11-15 12:56:31 +0700

    Output 1 & 2

---
+++ perulanganangkaa.java
@@ -0,0 +1,12 @@
+public class perulanganangkaa {
+	public static void main(String[] args) {
+		int i,j;
+
+		for (i =1; i <= 5; i++) {
+			System.out.println( "" );
+			for (j =1; j <= i; j++) {
+				System.out.print( j+" " );
+			}
+		}
+	}
+}
